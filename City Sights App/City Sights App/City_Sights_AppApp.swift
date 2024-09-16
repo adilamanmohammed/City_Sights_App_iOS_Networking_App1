da8c513613c15382f363c3742d2b3409730f1502
“adilamanmohammed” <adil.mohammed@okstate.edu>
2024-09-15 19:23:22 -0700

    - Added API_KEY

---
+++ City Sights App/City Sights App/City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_Sights_AppApp.swift
+//  City Sights App
+//
+//  Created by Adil Aman Mohammed on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City Sights App/City Sights App/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  CitySights App
+//
+//  Created by Adil Aman Mohammed on 8/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var query :String = ""
+    
+    var body: some View {
+            HStack{
+                
+                TextField("What are you Looking for ?", text: $query)
+                Button {
+                    // To Implement
+                }
+            label: {
+                Text("Goo on")
+                    .padding(.horizontal)
+                    .padding(.vertical, 10)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+        .onAppear(perform: {
+            print(Bundle.main.infoDictionary? ["API_KEY"] as? String)
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
